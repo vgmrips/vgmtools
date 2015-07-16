0147d6536ec9500f6318248c35452ef90b623532
vampirefrog <mihai.draghicioiu@gmail.com>
2015-07-16 23:28:08 +0300

    Added compat files

---
+++ compat.h
@@ -0,0 +1,46 @@
+/* Include this file before <stdio.h> */
+
+// VC6
+#if _MSC_VER == 1200
+// emulate stdint
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+
+// printf() formats
+#define FMT_UINT64 "%I64u"
+#define FMT_INT64 "%I64d"
+#define FMT_XINT64 "%I64X"
+
+// _getch()
+#include <conio.h>
+
+#else
+// integer types
+#include <stdint.h>
+
+// printf() formats
+#define FMT_UINT64 "%lu"
+#define FMT_INT64 "%ld"
+#define FMT_XINT64 "%lX"
+#endif
+
+#ifdef __STDC_LIB_EXT1__
+#define __STDC_WANT_LIB_EXT1__
+#include <stdio.h>
+#else
+#include <stdio.h>
+#include <stdlib.h>
+char *gets_s(char *str, size_t n);
+#endif
+
+#ifndef WIN32
+int _getch();
+#endif
+
+#ifdef _MSC_VER
+#define strdup _strdup
+#else
+#define _getch getchar
+#define _stricmp strcasecmp
+#define wcsicmp wcscasecmp
+#endif
