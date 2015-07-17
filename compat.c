be2b34f9d853747d9df41c582ee1387d03ad5169
vampirefrog <mihai.draghicioiu@gmail.com>
2015-07-18 00:24:37 +0300

    compat.c fix for VC6

--- compat.c
+++ compat.c
@@ -2,8 +2,9 @@
 #include <string.h>
 
 char *gets_s(char *str, size_t n) {
+	int l;
 	fgets(str, n, stdin);
-	int l = strlen(str);
+	l = strlen(str);
 	if(l > 0 && str[l-1] == '\n') str[l-1] = 0;
 	return str;
 }
