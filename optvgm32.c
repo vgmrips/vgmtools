981b24b3e746858a1c95589aca19c0f1a2b643ee
Brian Pow <brianpow@gmail.com>
2021-10-31 06:59:33 +0800

    fix possible buffer overflow vulnerabilities

--- optvgm32.c
+++ optvgm32.c
@@ -71,7 +71,7 @@ UINT32 VGMPos;
 INT32 VGMSmplPos;
 UINT8* DstData;
 UINT32 DstDataLen;
-char FileBase[0x100];
+char FileBase[MAX_PATH];
 UINT32 DataSizeA;
 UINT32 DataSizeB;
 
@@ -88,7 +88,7 @@ int main(int argc, char* argv[])
 {
 	int argbase;
 	int ErrVal;
-	char FileName[0x100];
+	char FileName[MAX_PATH];
 
 	printf("VGM PWM Optimizer\n-----------------\n\n");
 
@@ -103,7 +103,7 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		strcpy(FileName, argv[argbase + 0]);
+		strncpy(FileName, argv[argbase + 0], MAX_PATH-1);
 		printf("%s\n", FileName);
 	}
 	if (! strlen(FileName))
@@ -129,13 +129,12 @@ int main(int argc, char* argv[])
 	if (DataSizeB < DataSizeA)
 	{
 		if (argc > argbase + 1)
-			strcpy(FileName, argv[argbase + 1]);
+			strncpy(FileName, argv[argbase + 1], MAX_PATH-1);
 		else
 			strcpy(FileName, "");
 		if (FileName[0] == '\0')
 		{
-			strcpy(FileName, FileBase);
-			strcat(FileName, "_optimized.vgm");
+			snprintf(FileName, MAX_PATH, "%s_optimized.vgm", FileBase);
 		}
 		WriteVGMFile(FileName);
 	}
@@ -221,7 +220,7 @@ static bool OpenVGMFile(const char* FileName)
 
 	gzclose(hFile);
 
-	strcpy(FileBase, FileName);
+	strncpy(FileBase, FileName, MAX_PATH-1);
 	TempPnt = strrchr(FileBase, '.');
 	if (TempPnt != NULL)
 		*TempPnt = 0x00;
