vgmrips/vgmtools

commit 5c54bac71ab657a1cbb172e44ab957026847f1f2

fix issues RAintN22 ignored

vgm_trml.c
@@ -132,7 +132,7 @@ typedef struct rewrite_chipset_new
 	CHIP_DATA K007232;
 } REWRT_CHIPSET_NEW;
 
-#define CHIP_COUNT	0x2A
+#define CHIP_COUNT	0x2B
 
 static const char* CHIP_STRS[CHIP_COUNT] =
 {	"SN76496", "YM2413", "YM2612", "YM2151", "SegaPCM", "RF5C68", "YM2203", "YM2608",
@@ -140,14 +140,14 @@ static const char* CHIP_STRS[CHIP_COUNT] =
 	"RF5C164", "PWM", "AY8910", "GameBoy", "NES APU", "MultiPCM", "uPD7759", "OKIM6258",
 	"OKIM6295", "K051649", "K054539", "HuC6280", "C140", "K053260", "Pokey", "QSound",
 	"SCSP", "WSwan", "VSU", "SAA1099", "ES5503", "ES5506", "X1-010", "C352",
-	"GA20", "K007232"};
+	"GA20", "Mikey", "K007232"};
 static const char* CHIP_STRS2[CHIP_COUNT] =
 {	"", "", "", "", "", "", "", "",
 	"", "", "", "", "", "YMF278B/PCM", "", "",
 	"", "", "", "", "", "", "", "",
 	"", "", "", "", "", "", "", "",
 	"", "", "", "", "", "", "", "",
-	""};
+	"", "", ""};
 
 static UINT8 OPN_PRESCALER_REG_LIST[0x04][2] =
 {
@@ -908,7 +908,9 @@ static void SetImportantCommands(void)
 				break;
 			case 0x28:	// GA20
 				break;
-			case 0x2A:  // K007232
+			case 0x29:	// Mikey
+				break;
+			case 0x2A:	// K007232
 				break;
 			}
 		}
@@ -1743,9 +1745,11 @@ static void InitializeVGM(UINT8** DstDataRef, UINT32* DstPosRef)
 				ChipCmd = 0xBF;
 				CmdType = 0x12;
 				break;
-			case 0x2A:  // K007232
+			//case 0x29:	// Mikey
+			//	break;
+			case 0x2A:	// K007232
 				ChipCmd = 0x41;
-				CmdType = 0x80;
+				CmdType = 0x12;
 			default:
 				CmdType = 0xFF;
 				break;

vgm_cnt.c
@@ -31,7 +31,7 @@ static void DoChipCommand(UINT8 ChipSet, UINT8 ChipID, UINT16 Reg, UINT16 Data);
 static void DoKeyOnOff(CHIP_STATE* CState, UINT8 Chn, UINT8 OnOff, UINT8 VolFlag);
 
 
-#define CHIP_COUNT	0x2A
+#define CHIP_COUNT	0x2B
 
 VGM_HEADER VGMHead;
 UINT32 VGMDataLen;
@@ -295,6 +295,7 @@ static void CountVGMData()
 		case 0x1F:
 			TempLng = VGMHead.lngHzQSound;
 			break;
+		// TODO: 0x20 SCSP .. 0x29 Mikey
 		case 0x2A:
 			TempLng = VGMHead.lngHzK007232;
 			break;
@@ -571,6 +572,7 @@ static void CountVGMData()
 					case 0x8F:	// QSound ROM Image
 						DoChipCommand(CurChip, 0x1F, 0xFFFF, TempByt);
 						break;
+					// TODO: 0x91 X1-010 .. 0x93 GA20
 					case 0x94:	// K007232 ROM Image
 						DoChipCommand(CurChip, 0x2A, 0xFFFF, TempByt);
 						break;
@@ -1160,15 +1162,16 @@ static void DoChipCommand(UINT8 ChipSet, UINT8 ChipID, UINT16 Reg, UINT16 Data)
 			}
 		}
 		break;
-		case 0x2A:	// K007232
-			if (Reg == 0x1F)
-				Reg = Data;	// chip read - data value contains register ID
-			if (Reg == 0x05 || Reg == 0x0B)
-			{
-				UINT8 ch = (Reg == 0x05) ? 0 : 1;
-				DoKeyOnOff(TempChp, ch, 1, 0x00);
-			}
-			break;
+	// TODO: 0x20 SCSP .. 0x29 Mikey
+	case 0x2A:	// K007232
+		if (Reg == 0x1F)
+			Reg = Data;	// chip read - data value contains register ID
+		if (Reg == 0x05 || Reg == 0x0B)
+		{
+			UINT8 ch = (Reg == 0x05) ? 0 : 1;
+			DoKeyOnOff(TempChp, ch, 1, 0x00);
+		}
+		break;
 	}
 
 	return;

commit 1228c536516c9e73a863c6bbe7776d692c6abe79

vgm_trim: fix error introduced by 5c54bac
Fixes #39

vgm_trml.c
@@ -129,6 +129,7 @@ typedef struct rewrite_chipset_new
 	CHIP_DATA X1_010;
 	CHIP_DATA C352;
 	CHIP_DATA GA20;
+	CHIP_DATA Mikey;
 	CHIP_DATA K007232;
 } REWRT_CHIPSET_NEW;
 